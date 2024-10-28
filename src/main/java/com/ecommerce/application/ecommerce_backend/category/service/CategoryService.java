348320ed5ad87bb8318023dff20dc25abae7e224
Nika Oniani <nikaoniani2018@gmail.com>
2024-10-29 01:10:33 +0400

    Added a category endpoint which gets all the categories

---
+++ src/main/java/com/ecommerce/application/ecommerce_backend/category/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.ecommerce.application.ecommerce_backend.category.service;
+
+import java.util.List;
+import com.ecommerce.application.ecommerce_backend.category.Category;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+}
