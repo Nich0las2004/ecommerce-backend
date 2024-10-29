b8c37e49bc7187a6c9faf01231a9aba295517db6
Nika Oniani <nikaoniani2018@gmail.com>
2024-10-29 17:09:27 +0400

    Added a category endpoint for admin, which creates a category

--- src/main/java/com/ecommerce/application/ecommerce_backend/category/controller/CategoryController.java
+++ src/main/java/com/ecommerce/application/ecommerce_backend/category/controller/CategoryController.java
@@ -5,9 +5,8 @@ import com.ecommerce.application.ecommerce_backend.category.service.CategoryServ
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +25,11 @@ public class CategoryController {
     public ResponseEntity<List<Category>> getAllCategories(){
         List<Category> categories = categoryService.getAllCategories();
         return ResponseEntity.status(HttpStatus.OK).body(categories);
+    }
+
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Category createCategory(@RequestBody Category category){
+        return categoryService.createCategory(category);
     }
 }
