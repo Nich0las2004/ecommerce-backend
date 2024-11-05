a0ab02b58a040c15938314a8efc9c7c6710d877e
Nika Oniani <nikaoniani2018@gmail.com>
2024-11-05 14:08:12 +0400

    User can log in successfully, added CustomUserDetails and CustomUserDetailsService files, configured SecurityConfig

---
+++ src/main/java/com/ecommerce/application/ecommerce_backend/user/details/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.ecommerce.application.ecommerce_backend.user.details;
+
+import com.ecommerce.application.ecommerce_backend.user.User;
+import com.ecommerce.application.ecommerce_backend.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+}
